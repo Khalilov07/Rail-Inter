0943cf750a356cc74e3157b3c9225e3c16825f46
Akram <khalilovakram07@gmail.com>
2024-07-24 20:00:18 +0600

    added web site

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollToTopButton = document.getElementById("scrollToTop");
+
+  // Показать/скрыть кнопку в зависимости от прокрутки
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      scrollToTopButton.classList.add("visible");
+    } else {
+      scrollToTopButton.classList.remove("visible");
+    }
+  });
+
+  // Прокрутка вверх при клике на кнопку
+scrollToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
+
