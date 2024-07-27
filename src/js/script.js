7092606895e0899b6a8875fe13fe0d385ef547e7
Akram <khalilovakram07@gmail.com>
2024-07-27 18:24:04 +0600

    add new changes

--- src/js/script.js
+++ src/js/script.js
@@ -19,16 +19,38 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const currentLocation = window.location.pathname;
-  const menuItems = document.querySelectorAll('.nav-link');
+  const menuItems = document.querySelectorAll(".nav-link");
 
   console.log(currentLocation);
   console.log(menuItems);
 
-  menuItems.forEach(item => {
-      if(item.getAttribute('href') === currentLocation) {
-          item.classList.add('active');
-      }
+  menuItems.forEach((item) => {
+    if (item.getAttribute("href") === currentLocation) {
+      item.classList.add("active");
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const serviceBlocks = document.querySelectorAll(".service-block");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  serviceBlocks.forEach((block) => {
+    observer.observe(block);
   });
 });
